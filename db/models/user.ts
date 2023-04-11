adb319212d67aa6adc1bda05d43d35f6532a8e67
Matthew <mmartinezmateo93@gmail.com>
2023-04-11 22:35:47 +0200

    REV20230411 Added user model.

---
+++ db/models/user.ts
@@ -0,0 +1,39 @@
+import {Sequelize, DataTypes} from "sequelize";
+import {Model} from "./abstracts/model";
+
+export class ProductModel extends Model {
+    init(sequelize, relations = null) {
+        return this.instanceModel(
+            sequelize,
+            'User',
+            {
+                // Model attributes are defined here
+                id: {
+                    type: DataTypes.INTEGER,
+                    primaryKey: true,
+                    autoIncrement: true
+                },
+                username: {
+                    type: DataTypes.STRING,
+                    allowNull: false,
+                    unique: true
+                },
+                email: {
+                    type: DataTypes.STRING,
+                    allowNull: false,
+                    unique: true,
+                    validate: {
+                        isEmail: true
+                    }
+                },
+                password: {
+                    type: DataTypes.STRING,
+                    allowNull: false
+                }
+            },
+            'user',
+            relations
+        )
+    }
+
+}
