dc7991326b24448ef81cfe9e1893e6757171dc08
Matthew32 <mmartinezmateo93@gmail.com>
2023-05-10 22:52:52 +0200

    Upgrade

--- src/repositories/product.repository.ts
+++
@@ -1,8 +0,0 @@
-import db from "../../db/index"
-import {Repository} from "./abstracts/repository";
-
-export class ProductRepository extends Repository{
-    constructor() {
-        super(db.productModel);
-    }
-}
--- src/repositories/user.repository.ts
+++
@@ -1,8 +0,0 @@
-import db from "../../db/index"
-import {Repository} from "./abstracts/repository";
-
-export class UserRepository extends Repository {
-    constructor() {
-        super(db.userModel);
-    }
-}
